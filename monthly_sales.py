d25bf5f01a60d457ff238ad1b1b8be3d500c70cd
Caroline Feeney <carolinefeeney@Carolines-MacBook-Pro-5.local>
2019-02-12 00:14:53 -0500

    Found unique products

--- monthly_sales.py
+++ monthly_sales.py
@@ -17,14 +17,27 @@ csv_filename = "sales-201710.csv" #TODO allow user to specify with FILEPATH
 # ... adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
 csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)  #> reference a file in the data directory
 # ... this and other pandas operations adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
-csv_data = csv_data = pandas.read_csv(csv_filepath) #> read CSV into pandas dataframe object
+csv_data = csv_data = pandas.read_csv(csv_filepath) #> read CSV into pandas dataframe object[df object is now "csv_data"]
 
 #
 # CALCULATIONS
 #
 
-monthly_total = csv_data["sales price"].sum() #> from the csv data, sum the sales prices to get the monthly total
+monthly_total = csv_data["sales price"].sum() #> from the dataframe we just made, sum the sales prices to get the monthly total
 #print(monthly_total)
+
+# ... adapted from https://github.com/s2t2/exec-dash-starter-py/commit/f790f124895db77920e37655c91e1e5a7a424aaa
+product_names = csv_data["product"] #> from dataframe, list only the product name
+#print(product_names)
+#print(type(product_names))
+#> this is 'pandas.core.series.Series'
+unique_product_names = product_names.unique()
+#print(unique_product_names)
+#print(type(unique_product_names))
+#> this is  'numpy.ndarray' > google how to convert to a list
+unique_product_names = unique_product_names.tolist() # convert numpy.ndarray to list
+
+
 
 
 
